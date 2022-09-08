4fdcf6c71096fc53be41523d228384475b3bfe12
Michele M <mnmin@gmail.com>
2022-09-08 18:29:27 +0100

    Movie - server

--- src/server/controllers/movie.js
+++ src/server/controllers/movie.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = process.env.JWT_SECRET;
 
 const getAllMovies = async (req, res) => {
     const movies = await prisma.movie.findMany();
@@ -13,16 +13,36 @@ const getAllMovies = async (req, res) => {
 const createMovie = async (req, res) => {
     const { title, description, runtimeMins } = req.body;
 
+    const [_, token] = req.get('Authorization').split(' ');
+    
+        console.log(title)
+        console.log(description)
+        console.log(runtimeMins)
+        console.log(_)
+        console.log(token)
+
     try {
-        const token = null;
         // todo verify the token
+        const decoded = jwt.verify(token, jwtSecret)
     } catch (e) {
         return res.status(401).json({ error: 'Invalid token provided.' })
     }
 
-    const createdMovie = null;
+    try {
 
-    res.json({ data: createdMovie });
+        const createdMovie = await prisma.movie.create({
+            data: {
+                title: title,
+                description: description,
+                runtimeMins: runtimeMins
+            },
+        });
+        console.log("new movie", createdMovie)
+        return res.status(201).json({ data: createdMovie})
+    } catch (e) {
+        return res.status(401).json({ error: e.message })
+    }
+
 };
 
 module.exports = {
